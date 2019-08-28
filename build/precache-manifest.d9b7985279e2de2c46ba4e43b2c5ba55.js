a95d49d31ce5d7e45ea2f759dc042c9f23a16edd
sbuteryd@qq.com <sbuteryd@qq.com>
2019-08-28 18:37:41 +0800

    10-build

---
+++ build/precache-manifest.d9b7985279e2de2c46ba4e43b2c5ba55.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e97d5061f6ed482b3e3b41fc4a2b39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dcc69c06d98cc187b9d",
+    "url": "/static/js/2.829483cb.chunk.js"
+  },
+  {
+    "revision": "a99b09a745479e0ee00e",
+    "url": "/static/js/main.f597d175.chunk.js"
+  },
+  {
+    "revision": "9a3b7e65940f63df24ec",
+    "url": "/static/js/runtime~main.1e8ea06d.js"
+  }
+]);
