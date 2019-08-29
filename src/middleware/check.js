e1a5c33ccab38bc6cfc976c7d3148d2baf4f90fc
sbuteryd@qq.com <sbuteryd@qq.com>
2019-08-29 19:30:16 +0800

    3-add middleware

---
+++ src/middleware/check.js
@@ -0,0 +1,19 @@
+import {
+    ADD_TODO
+} from '../action/todos'
+import {
+    ADD_GOALS
+} from '../action/goals'
+
+const check = (store)=>(next)=>(action)=>{
+    if(action.type ===ADD_TODO
+        && action.todo.name
+            .toLowerCase()
+            .includes('bitcoin')){
+        return alert('That is bad ideal')
+    }
+    if(action.type ===ADD_GOALS && action.goal.name.toLowerCase().includes('bitcoin')){
+        return  alert("That is bad ideal")
+    }
+    return next(action)
+}
